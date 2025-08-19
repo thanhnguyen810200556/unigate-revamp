6dfcf61c648f4e613a26e393634c3a1cb11de5d9
Thanh Nguyễn <thanhnguyen20051457@gmail.com>
2025-08-19 22:49:24 +0700

    Update App.jsx
    feat: apply ProtectedRoute to secure routes in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,49 @@
+import { Route, Routes } from "react-router-dom";
 import HomepageLayout from "./shared/layout/HomepageLayout/HomepageLayout";
 import HomePage from "./features/home/pages/HomePage";
 import ContactPage from "./features/contact/pages/ContactPage";
 import NewsPage from "./features/news/pages/NewsPage";
-import { Route, Routes } from "react-router-dom";
-import LoginPage from "./features/auth/pages/loginPage";
-import Dashboard from "./features/studentDashboard/Pages/dashboard";
+import LoginPage from "./features/auth/pages/LoginPage";
+import Dashboard from "./features/studentDashboard/Pages/Dashboard";
 import DashboardLayout from "./shared/layout/DashboardLayout/DashboardLayout";
 import ProfilePage from "./features/studentProfile/pages/ProfilePage";
 import ExamPage from "./features/exam/pages/ExamPage";
-import DocumentsPage from "./features/documents/pages/DocumentsPage";
 import AIChatPage from "./features/aiChat/pages/AIChatPage";
-import AdmissionPage from "./features/admission/pages/AdmissionPage";
-import ForumPage from "./features/forum/pages/ForumPage";
+import ExamTest from "./features/examTest/pages/examTest";
+import DetailsNewsPage from "./features/news/pages/DetailsNewsPage";
+import ProtectedRoute from "./shared/components/ProtectedRoute";
 function App() {
   return (
     <Routes>
       <Route path="/" element={<HomepageLayout />}>
-        <Route path="/" element={<HomePage />} />
+        <Route index element={<HomePage />} />
         <Route path="news" element={<NewsPage />} />
+        <Route path="news/:id" element={<DetailsNewsPage />} />
         <Route path="contact" element={<ContactPage />} />
         <Route path="login" element={<LoginPage />} />
       </Route>
 
-      <Route path="/dashboard" element={<DashboardLayout />}>
-        <Route path="home" element={<Dashboard />} />
+      <Route
+        path="/dashboard"
+        element={
+          <ProtectedRoute>
+            <DashboardLayout />
+          </ProtectedRoute>
+        }
+      >
+        <Route index element={<Dashboard />} />
         <Route path="profile" element={<ProfilePage />} />
         <Route path="exam" element={<ExamPage />} />
-        <Route path="document" element={<DocumentsPage />} />
         <Route path="ai-chat" element={<AIChatPage />} />
-        <Route path="admission" element={<AdmissionPage />} />
-        <Route path="forum" element={<ForumPage />} />
       </Route>
+      <Route
+        path="/dashboard/exam-test"
+        element={
+          <ProtectedRoute>
+            <ExamTest />
+          </ProtectedRoute>
+        }
+      />
     </Routes>
   );
 }
